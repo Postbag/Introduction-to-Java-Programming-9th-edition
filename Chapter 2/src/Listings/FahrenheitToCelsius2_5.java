883883ab1c6c80a9c211440a705794a57ea5a04a
Philipp <hazebag@gmail.com>
2012-08-03 11:23:19 +0200

    Quick update on FahrenheitToCelcius2_5

--- Chapter 2/src/Listings/FahrenheitToCelsius2_5.java
+++ Chapter 2/src/Listings/FahrenheitToCelsius2_5.java
@@ -1,8 +1,10 @@
 package Listings;
 
+import java.util.Scanner;
+
 public class FahrenheitToCelsius2_5 {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
 	}
 
 }
